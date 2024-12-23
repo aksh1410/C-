b652e2bf6a5ab9f565943f3f079e77c423036709
Akash sharma <121504247+aksh1410@users.noreply.github.com>
2024-12-23 19:35:12 +0530

    some codes of different types of pattern problems

--- 04_controlstatements]/03_forloop.cpp
+++ 04_controlstatements]/03_forloop.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int i,j;
+//int main(){
+  //  int i,j;
     // for(i=1;i<=4;i++){
     //     for(j=1;j<=3;j++){
     //         cout << "*";
@@ -18,15 +18,32 @@ int main(){
     //     cout << endl;
 
     //  }
-    for(i=4;i<=4;i--){
-        for(j=1;j<=i;j++){
-            cout <<"*";
+    //reverse triangle
+    // for(i=4;i<=4;i--){
+    //     for(j=1;j<=i;j++){
+    //         cout <<"*";
+    //     }
+    //     cout << endl;
+
+    // }
+    //triangl with spaces
+
+int main(){
+    int  num,i,j,k;
+    cout << "enter the length of triangle: ";
+    cin >> num;
+    for(i=1;i<=num;i++){
+        for(j=num;j>i;j--){
+            cout << " ";
+            }
+            for(k=1;k<=i;k++){
+                cout << "*";
         }
         cout << endl;
-
     }
+}
 
    
     
-}
+
 
